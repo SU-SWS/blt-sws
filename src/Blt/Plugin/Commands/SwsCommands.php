de67032e11c8c2c3a844d3f0dcb27c56f5ad870d
Mike Decker <pookmish@gmail.com>
2024-02-29 08:46:25 -0800

    Add site update command for ACE site updates

--- src/Blt/Plugin/Commands/SwsCommands.php
+++ src/Blt/Plugin/Commands/SwsCommands.php
@@ -4,6 +4,7 @@ namespace Sws\BltSws\Blt\Plugin\Commands;
 
 use Acquia\Blt\Robo\BltTasks;
 use Acquia\Blt\Robo\Exceptions\BltException;
+use GuzzleHttp\Client;
 use Robo\ResultData;
 
 /**
@@ -364,6 +365,105 @@ class SwsCommands extends BltTasks {
       ->getMessage();
     $this->siteAliases = json_decode($aliases, TRUE);
     return $this->siteAliases;
+  }
+
+  /**
+   * After code deployed, update all sites on the stack.
+   *
+   * @command sws:post-code-deploy
+   *
+   * @aliases sws:post-code-update
+   */
+  public function postCodeDeployUpdate($target_env, $deployed_tag) {
+    $sites = $this->getConfigValue('multisites');
+    $parallel_executions = (int) getenv('UPDATE_PARALLEL_PROCESSES') ?: 10;
+
+    $site_chunks = array_chunk($sites, ceil(count($sites) / $parallel_executions));
+    $commands = [];
+    foreach ($site_chunks as $sites) {
+      $commands[] = $this->blt()
+        ->arg('sws:update-sites')
+        ->arg(implode(',', $sites))
+        ->getCommand();
+    }
+    file_put_contents(sys_get_temp_dir() . '/update-report.txt', '');
+    $this->taskExec(implode(" &\n", $commands) . PHP_EOL . 'wait')->run();
+    $report = array_filter(explode("\n", file_get_contents(sys_get_temp_dir() . '/update-report.txt')));
+
+    $success = [];
+    $failed = [];
+    foreach ($report as $line) {
+      [$site, $status] = explode(':', $line);
+      if ((int) $status) {
+        $success[] = $site;
+      }
+      else {
+        $failed[] = $site;
+      }
+    }
+    unlink(sys_get_temp_dir() . '/update-report.txt');
+
+    $this->yell(sprintf('Updated %s sites successfully.', count($success)), 100);
+    $slack_url = getenv('SLACK_NOTIFICATION_URL');
+
+    if ($failed) {
+      $this->yell(sprintf("Update failed for the following sites:\n%s", implode("\n", $failed)), 100, 'red');
+
+      if ($slack_url) {
+        $count = count($failed);
+        $this->sendSlackNotification($slack_url, "A new deployment has been made to *$target_env* using *$deployed_tag*.\n\n*$count* sites failed to update.");
+      }
+      throw new \Exception('Failed update');
+    }
+
+    if ($slack_url) {
+      $this->sendSlackNotification($slack_url, "A new deployment has been made to *$target_env* using *$deployed_tag*.");
+    }
+  }
+
+  /**
+   * Send out a slack notification.
+   *
+   * @param string $message
+   *   Slack message.
+   */
+  protected function sendSlackNotification(string $slack_webhook, string $message) {
+    $client = new Client();
+    $client->post($slack_webhook, [
+      'form_params' => [
+        'payload' => json_encode([
+          'username' => 'Acquia Cloud',
+          'text' => $message,
+          'icon_emoji' => 'information_source',
+        ]),
+      ],
+    ]);
+  }
+
+  /**
+   * Run db updates and config imports to a list of sites.
+   *
+   * @command sws:update-sites
+   *
+   * @var string $sites
+   *   Comma delimited list of sites to update.
+   */
+  public function updateSites($sites, $options = ['partial-config-import' => FALSE]) {
+    $sites = explode(',', $sites);
+    foreach ($sites as $site_name) {
+      $this->switchSiteContext($site_name);
+      $task = $this->taskDrush();
+      if (!$options['partial-config-import']) {
+        $task->drush('updatedb')
+          ->drush('config:import')
+          ->option('partial');
+      }
+      else {
+        $task->drush('deploy');
+      }
+      $success = $task->run()->wasSuccessful();
+      file_put_contents(sys_get_temp_dir() . '/update-report.txt', $site_name . ($success ? ':1' : ':0') . PHP_EOL, FILE_APPEND);
+    }
   }
 
 }
